df9129f2b4a9f6e267e1d88d37dacf4cd6175438
Ang Li <angli@Angs-MacBook-Pro.local>
2016-12-13 18:09:35 -0600

    add header file

---
+++ query_spelling/main.cpp
@@ -0,0 +1,20 @@
+#include "querycorrector.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  QueryCorrector queryCorrector;
+  while (1) {
+    cout << "Enter a query:" << endl;
+    string query;
+    getline(cin, query);
+    auto res = queryCorrector.generateCorrections(query);
+    cout << "Suggested corrections:" << endl;
+    for (auto line : res) {
+      cout << line.first + ' ' << line.second[0] << endl;
+    }
+  }
+}
---
+++ query_spelling/querycorrector.h
@@ -0,0 +1,24 @@
+#ifndef QUERYCORRECTOR_H
+#define QUERYCORRECTOR_H
+
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class QueryCorrector
+{
+  public:
+    QueryCorrector();
+    pair<double, double> discriminativeTraining(int training_size, int iterations);
+    vector<pair<string, vector<double>>> generateCorrections(string query);
+  private:
+   double lambda = 0.5;
+   double mu = 1.0;
+
+   double computeScore(string word1, string word2, double emission);
+   double compScore(double transition, double emission);
+   void runTestData();
+};
+#endif
+    
